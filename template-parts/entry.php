abb1ba06e411e04b9bd1e24712e7a2ce602c5652
robertandrews <mail@robertandrews.co.uk>
2022-05-28 07:42:23 +0100

    Make article a .card .p-4

--- template-parts/entry.php
+++ template-parts/entry.php
@@ -1,4 +1,4 @@
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class('card p-4'); ?>>
 <header>
 <?php if ( is_singular() ) { echo '<h1 class="entry-title" itemprop="headline">'; } else { echo '<h2 class="entry-title">'; } ?>
 <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php the_title(); ?></a>
