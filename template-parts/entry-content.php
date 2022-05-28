a6d0f0dc0f0ac170d981902f2793217ca3e50e0e
robertandrews <mail@robertandrews.co.uk>
2022-05-28 07:29:35 +0100

    Make featured image responsive with .img-fluid

--- template-parts/entry-content.php
+++ template-parts/entry-content.php
@@ -1,6 +1,6 @@
 <div class="entry-content" itemprop="mainEntityOfPage">
 <?php if ( has_post_thumbnail() ) : ?>
-<a href="<?php $src = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full', false ); echo esc_url( $src[0] ); ?>" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail( 'full', array( 'itemprop' => 'image' ) ); ?></a>
+<a href="<?php $src = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full', false ); echo esc_url( $src[0] ); ?>" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail( 'full', array( 'itemprop' => 'image', 'class' => 'img-fluid' ) ); ?></a>
 <?php endif; ?>
 <meta itemprop="description" content="<?php echo esc_html( wp_strip_all_tags( get_the_excerpt(), true ) ); ?>" />
 <?php the_content(); ?>
